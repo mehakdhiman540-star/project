03142ca9fcfdbdf19d529adbd4d39e3a76af30bf
mehak <mehakdhiman540@gmail.com>
2026-05-11 13:18:53 +0530

    bank

---
+++ BankDemo.java
@@ -0,0 +1,30 @@
+abstract class Bank {
+
+    abstract int getRateOfInterest();
+}
+
+class SBI extends Bank {
+
+    int getRateOfInterest() {
+        return 7;
+    }
+}
+
+class HDFC extends Bank {
+
+    int getRateOfInterest() {
+        return 8;
+    }
+}
+
+public class BankDemo {
+
+    public static void main(String[] args) {
+
+        SBI s = new SBI();
+        HDFC h = new HDFC();
+
+        System.out.println("SBI Interest: " + s.getRateOfInterest());
+        System.out.println("HDFC Interest: " + h.getRateOfInterest());
+    }
+}
