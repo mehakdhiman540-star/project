82d69cf49ded63483f9bd02f59bd028b94cd4c66
mehak <mehakdhiman540@gmail.com>
2026-05-12 21:58:56 +0530

    string3

---
+++ EmptyString.java
@@ -0,0 +1,13 @@
+public class EmptyString {
+
+    public static void main(String[] args) {
+
+        String str = "";
+
+        if(str.isEmpty()) {
+            System.out.println("String is empty");
+        } else {
+            System.out.println("String is not empty");
+        }
+    }
+}
