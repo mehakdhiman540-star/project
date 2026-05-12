de7a4ff30d0e5a4a82632c1379bcca87b6fb5725
mehak <mehakdhiman540@gmail.com>
2026-05-12 21:57:35 +0530

    string2

---
+++ CharacterCount.java
@@ -0,0 +1,11 @@
+public class CharacterCount {
+
+    public static void main(String[] args) {
+
+        String str = "Programming";
+
+        int count = str.length();
+
+        System.out.println("Characters = " + count);
+    }
+}
